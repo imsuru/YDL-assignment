e52bfe163f3c8a9e369525f991769d7927387fd6
imsuru <suraj.nayak@student.vishwaniketan.edu.in>
2024-12-24 02:55:51 +0530

    changes done in package.json

--- vite.config.js
+++ vite.config.js
@@ -13,3 +13,11 @@ export default defineConfig({
     },
   },
 });
+
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+
+// export default defineConfig({
+//   plugins: [react()],
+//   base: '/<repository-name>/', // Replace <repository-name> with your repository name
+// });
